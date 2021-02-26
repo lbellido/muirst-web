01cadd611837372437329ab474884c0aec21c5b8
Luis Bellido Triana <luis.bellido@upm.es>
2021-02-26 19:50:26 +0100

    Add readme and deploy

---
+++ deploy.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+allflag=
+
+while getopts ":a" option
+do
+    case $option in
+    a)    allflag=1;;
+    ?)    echo "Usage: $0 [-a]\n -a: upload both public and draft webs\n"; exit;;
+    esac
+done
+
+if [ ! -z "$allflag" ]; then
+echo "***********************************************************"
+echo "* This action will overwrite the current dist public web *"
+read -r -p "* Are you sure? [y/N] " response
+case $response in
+    ([yY][eE][sS]|[yY]) 
+      ;;
+    *)
+      exit;;
+esac
+fi
+
+TUNNEL="ssh -p 15001 -l lbt idefix.dit.upm.es ssh"
+LOCALDIR="muirst-local/"
+PUBLICREMOTE="posgrado:/home/posgrado/lib/www/muirst"
+DRAFTREMOTE="posgrado:/home/posgrado/lib/www/muirst-draft"
+
+# Create public site using ssh forwarding tunnel 
+if [ ! -z "$allflag" ]; then
+echo "Creating public site dit.upm.es/posgrado/muirst"
+rsync -avz -e "$TUNNEL" --delete $LOCALDIR $PUBLICREMOTE
+fi
+
+# Create draft site using ssh forwarding tunnel 
+echo "Creating draft site dit.upm.es/posgrado/muirst-draft"
+rsync -avz -e "$TUNNEL" --delete $LOCALDIR $DRAFTREMOTE
+
+exit 0
+
